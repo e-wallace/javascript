b209ee8acf76e1aa0f9224f233713c614782f8bc
e-wallace <44043492+e-wallace@users.noreply.github.com>
2022-02-19 16:47:52 -0500

    Create p1_raceday.js

---
+++ p1_raceday.js
@@ -0,0 +1,17 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const registeredEarly = true;
+
+let runnerAge = 25;
+
+if (runnerAge > 18 && registeredEarly == true){
+  raceNumber +=1000;
+} else if (runnerAge > 18 && registeredEarly == true){
+  console.log(`You will be racing at 9:30am. Your race number is ${raceNumber}`)
+} else if (runnerAge > 18 && registeredEarly == false){
+  console.log(`You will be racing at 11am. Your race number is ${raceNumber}`);
+} else if (runnerAge < 18) {
+  console.log(`Youth registrants run at 12:30 pm. Your race number is ${raceNumber}`);
+} else {
+  console.log('Go see the registration desk.');
+}
